44ee0d3e21f6e3bbc7f2b7e858f58d3473c2551d
boraayhan <boraayhanwebsite@gmail.com>
2024-11-23 23:51:23 -0700

    added basic joystick functionality

--- RCFlightController.ino
+++ RCFlightController.ino
@@ -1,13 +1,15 @@
 #include <Servo.h>
 
-#define MAX_F_ANGLE 35 //Maximum flaperon angle. Higher values are less stable but allow for more freedom of motion. Does NOT include flaps 
+#define MAX_F_ANGLE 35  //Maximum flaperon angle. Higher values are less stable but allow for more freedom of motion. Does NOT include flaps
+#define FLAP_ANGLE 25
 
 #define VRX_PIN A0
 #define VRY_PIN A1
 #define JBUTTON_PIN 2
 
-int jX = 0;
-int jY = 0;
+double jX = 0;
+double jY = 0;
+int flap = 0;
 
 Servo f1;  //Flaperon
 Servo f2;
@@ -18,7 +20,7 @@ float f2_a0 = 90;
 void setup() {
   Serial.begin(9600);
   f1.attach(8);
-  f1.write(f1_a0);
+  pinMode(JBUTTON_PIN, INPUT);
 }
 
 void loop() {
@@ -34,8 +36,9 @@ void TestFlaperons(float ang) {
 }
 
 void UpdateControls() {
-  jX = constrain(analogRead(VRX_PIN)-500, -500, 500);
-  jY = constrain(analogRead(VRY_PIN)-500, -500, 500);
+  jX = constrain(analogRead(VRX_PIN) - 500, -400, 400);
+  jY = constrain(analogRead(VRY_PIN) - 500, -400, 400);
   Serial.println(jX);
   //Serial.println(jY);
+  f1.write(f1_a0 + jX / 400 * MAX_F_ANGLE - flap);
 }
