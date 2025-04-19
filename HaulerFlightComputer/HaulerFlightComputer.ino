29142615cb65a72ba90ca15eab8e08063e7029b5
boraayhan <boraayhanwebsite@gmail.com>
2025-04-19 10:23:47 -0600

    added more fx

--- HaulerFlightComputer/HaulerFlightComputer.ino
+++ HaulerFlightComputer/HaulerFlightComputer.ino
@@ -7,15 +7,15 @@
 
 #define RADIO_PIN_CE 7
 #define RADIO_PIN_CSN 8
-// CONSTANTS
 
+// CONSTANTS
 const float AILERON_ANGLE_MIN = -40;
 const float AILERON_ANGLE_MAX = 40;
 
 const float FLAP_ANGLE_MAX = 0;
 const float FLAP_ANGLE_MIN = -40;
 
-enum SurfaceID {W
+enum SurfaceID {
   AILERON_LEFT,
   AILERON_RIGHT,
   FLAP_LEFT,
@@ -24,7 +24,6 @@ enum SurfaceID {W
 };
 
 uint8_t address[][6] = { "1Node", "2Node" };
-
 
 struct Payload {
   int32_t id;
@@ -70,23 +69,20 @@ struct ControlSurface {
 };
 
 // VARIABLES AND OBJECTS
-
 RF24 radio(RADIO_PIN_CE, RADIO_PIN_CSN);
 
 ControlSurface surfaces[_num] = {
-  { Servo(), 2, 90, AILERON_ANGLE_MIN, AILERON_ANGLE_MAX, 1 },  // AILERON_LEFT
+  { Servo(), 2, 90, AILERON_ANGLE_MIN, AILERON_ANGLE_MAX, -1 },  // AILERON_LEFT
   //{Servo(), 3, 90, AILERON_ANGLE_MIN, AILERON_ANGLE_MAX, 1 },  // AILERON_RIGHT
   { Servo(), 4, 80, FLAP_ANGLE_MIN, FLAP_ANGLE_MAX, -1 },  // FLAP_LEFT
   //{Servo(), 5, 90, FLAP_ANGLE_MIN, FLAP_ANGLE_MAX, 1 }         // FLAP_RIGHT
 };
 
 float flap = 0;
-
 Payload payload;
 
 // FUNCTIONS
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
   InitializeSystems();
   Serial.println("Initialized!");
@@ -102,7 +98,7 @@ void InitializeSystems() {
   }
 }
 
-void SetupRadio() {
+void SetupRadio() {  // Initializes radio
   if (!radio.begin()) {
     Serial.println(F("Error: Radio hardware failure!"));
     while (1) {}
@@ -113,7 +109,7 @@ void SetupRadio() {
   radio.startListening();
 }
 
-void ReceiveRadio() {
+void ReceiveRadio() {  // Receives radio payload {id, p1, p2}, processes accordingly
   uint8_t pipe;
   if (radio.available(&pipe)) {
     Payload payload;
@@ -122,18 +118,15 @@ void ReceiveRadio() {
     float p2 = payload.p2;
     switch (payload.id) {
       case 0:  //Joystick input
-      processJoystick(p1, p2);
+        MoveSurfacesWithJoystick(p1, p2);
         break;
       case 1:  // set flap
         flap = p1;
-        surfaces[FLAP_LEFT].move(flap);
-        //surfaces[FLAP_RIGHT].move(flap);
+        MoveFlaps();
         break;
       case 2:  // delta flap
-        //flaps.move(flap + p1);
         flap += p1;
-        surfaces[FLAP_LEFT].move(flap);
-        //surfaces[FLAP_RIGHT].move(flap);
+        MoveFlaps();
         break;
       case 3:  // thrust set
                // engine1.write(payload.p1);
@@ -150,7 +143,13 @@ void ReceiveRadio() {
   }
 }
 
-void processJoystick(float rX, float rY) {
-  surfaces[AILERON_LEFT].move(rX * (AILERON_ANGLE_MAX - AILERON_ANGLE_MIN));
-  //Serial.println(rX*(AILERON_ANGLE_MAX - AILERON_ANGLE_MIN));
+void MoveSurfacesWithJoystick(float jX, float jY) {  // Translates payload data to aileron and elevator motion
+  float pAileron = jX * (AILERON_ANGLE_MAX - AILERON_ANGLE_MIN);
+  surfaces[AILERON_LEFT].move(pAileron);
+  surfaces[AILERON_RIGHT].move(pAileron);
+}
+
+void MoveFlaps() {  // Translates payload data to aileron and elevator motion
+  surfaces[FLAP_LEFT].move(flap);
+  surfaces[FLAP_RIGHT].move(flap);
 }
