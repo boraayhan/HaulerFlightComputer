5a22aca99ff2a0994da7c7538fee72a6eee9d9bb
boraayhan <boraayhanwebsite@gmail.com>
2025-05-26 21:05:34 -0600

    why doesnt switch:case work :sob:

--- HaulerFlightComputer/HaulerFlightComputer.ino
+++ HaulerFlightComputer/HaulerFlightComputer.ino
@@ -8,8 +8,10 @@
 #define RADIO_PIN_CE 7
 #define RADIO_PIN_CSN 8
 
+#define PROPELLER_PIN 49
+
 // CONSTANTS
-const float FLAPERON_RATIO_CONSTANT = 0; // 0 for flaperon mode off, 0.3 recommended  
+const float FLAPERON_RATIO_CONSTANT = 0;  // 0 for flaperon mode off, 0.3 recommended
 
 const float AILERON_POS_MIN = -40;
 const float AILERON_POS_MAX = 40;
@@ -70,9 +72,9 @@ struct ControlSurface {
     move(0);
   }
 
-  void move(float angle) {                                                                               //Moves to specified angle, accounting for zero-level
+  void move(float angle) {                                                               //Moves to specified angle, accounting for zero-level
     float target = constrain((angle + flap * FLAPERON_RATIO_CONSTANT), min, max) * dir;  //TODO: Verify
-    Serial.println(target);
+    //Serial.println(target);
     servo.write(zero + target);
   }
 };
@@ -84,10 +86,10 @@ Servo propeller;  // This is NOT a servo lmao
 bool active = false;
 
 ControlSurface surfaces[_num] = {
-  { Servo(), 2, 90, AILERON_POS_MIN, AILERON_POS_MAX, -1 },    // AILERON_LEFT
-  { Servo(), 3, 90, AILERON_POS_MIN, AILERON_POS_MAX, 1 },     // AILERON_RIGHT
-  { Servo(), 4, 80, FLAP_POS_MIN, FLAP_POS_MAX, -1 },          // FLAP_LEFT
-  { Servo(), 5, 90, FLAP_POS_MIN, FLAP_POS_MAX, 1 },           // FLAP_RIGHT
+  { Servo(), 2, 90, AILERON_POS_MIN, AILERON_POS_MAX, -1 },   // AILERON_LEFT
+  { Servo(), 3, 90, AILERON_POS_MIN, AILERON_POS_MAX, 1 },    // AILERON_RIGHT
+  { Servo(), 4, 80, FLAP_POS_MIN, FLAP_POS_MAX, -1 },         // FLAP_LEFT
+  { Servo(), 5, 90, FLAP_POS_MIN, FLAP_POS_MAX, 1 },          // FLAP_RIGHT
   { Servo(), 6, 90, ELEVATOR_POS_MIN, ELEVATOR_POS_MAX, 1 },  // ELEVATOR_LEFT
 };
 
@@ -100,12 +102,13 @@ void setup() {
 
 void loop() {
   ReceiveRadio();
-  if(!active) {
+  if (!active) {
     SetThrottle(0);
   }
 }
 void InitializeSystems() {
   SetupRadio();
+  propeller.attach(PROPELLER_PIN);
   for (ControlSurface& s : surfaces) {
     s.init();
   }
@@ -116,7 +119,7 @@ void SetupRadio() {  // Initializes radio
     Serial.println(F("Error: Radio hardware failure!"));
     while (1) {}
   }
-  radio.setPALevel(RF24_PA_LOW);
+  radio.setPALevel(RF24_PA_LOW); 
   radio.setPayloadSize(sizeof(Payload));
   radio.openReadingPipe(1, address[0]);
   radio.startListening();
@@ -125,33 +128,36 @@ void SetupRadio() {  // Initializes radio
 void ReceiveRadio() {  // Receives radio payload {id, p1, p2}, processes accordingly
   uint8_t pipe;
   if (radio.available(&pipe)) {
-    Payload payload;
     radio.read(&payload, sizeof(Payload));  // Receive the payload
     float p1 = payload.p1;
     float p2 = payload.p2;
-    switch (payload.id) {
-      case 0:  //Joystick input
-        MoveSurfacesWithJoystick(p1, p2);
-        break;
-      case 2:  // delta flap
-        flap += p1 * abs(FLAP_POS_MIN) / 3;
-        flap = constrain(flap, FLAP_POS_MIN, FLAP_POS_MAX);
-        MoveFlaps();
-        break;
-      case 3:  // Throttle
-        float speed = map(p1, 0, 1, 0, 180);
-        SetThrottle(speed);
-        break;
-      case 4:  // test surfaces
-        for (ControlSurface& s : surfaces) {
-          s.test();
-          Serial.println("Testing surface");
-          delay(500);
-        }
-        break;
-      case 5:  // Enable/Disable engine
-        active = !active;
-        break;
+    if (payload.id == 0) {  // Joystick input
+      MoveSurfacesWithJoystick(payload.p1, payload.p2);
+    }
+
+    if (payload.id == 2) {  // delta flap
+      flap += payload.p1 * abs(FLAP_POS_MIN) / 3;
+      flap = constrain(flap, FLAP_POS_MIN, FLAP_POS_MAX);
+      MoveFlaps();
+    }
+
+    if (payload.id == 3) {  // Throttle
+      float speed = payload.p1 * 180;
+      SetThrottle(speed);
+      Serial.println(speed);
+    }
+
+    if (payload.id == 4) {  // test surfaces
+      for (ControlSurface& s : surfaces) {
+        s.test();
+        Serial.println("Testing surface");
+        delay(500);
+      }
+    }
+
+    if (payload.id == 5) {  // Enable/Disable engine
+      active = !active;
+      Serial.println("active");
     }
   }
 }
@@ -173,8 +179,7 @@ void MoveFlaps() {  // Updates flap level
 void SetThrottle(float speed) {
   if (active) {
     propeller.write(speed);
-  }
-  else {
+  } else {
     propeller.write(0);
   }
 }
